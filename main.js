18f31c3a4667aa3b82bec7d696085fda2eeca4a5
thedesh123 <91805730+thedesh123@users.noreply.github.com>
2022-06-21 15:32:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Create a reference for the canvas
 
 canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext('2d');
+ctx = canvas.getContext("2d");
 
 img_width = 300;
 img_height = 100;
@@ -22,21 +22,68 @@ function uploadimg() {
 	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-//Write a code to grab the key-pressed event
-window.addEventListener ("keydown", my_keydown);
+window.addEventListener("keydown", my_keydown);
+
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
-	{
-		aplhabetkey();
-		document.getElementById('d1').innerHTML="you pressed Alphabet Key";
-		console.log("alphabet key");
-	}
-			otherkey();
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+			console.log("alphabet key");
+		}
+		else if(keyPressed >=48 && keyPressed<=57)
+		{
+			numberkey();
+			document.getElementById("d1").innerHTML="You pressed Number Key";
+			console.log("Number key");
+		}
+		else if(keyPressed >=37 && keyPressed<=40)
+		{
+			arrowkey();
+			document.getElementById("d1").innerHTML="You pressed Arrow Key";
+			console.log("Arrow Key");
+		}
+		else if((keyPressed ==17)|| (keyPressed ==18 || keyPressed ==27))
+		{
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed ctrl/esc/alt";
+			console.log("special key");
+		}
+	else{
+		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+		img_image="Alpkey.png";
+	add();
+}
+function numberkey()
+{
+	img_image="numkey.png";
+	add();
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
 	
 
 }
